5a2a1e3d41564895353ad792b8cc3b3755b58f83
BissyClassic <rachaelafolabi2021@gmail.com>
2025-09-24 12:39:32 +0100

    Add updated files and folders

--- alx-project-0x02/pages/home.tsx
+++ alx-project-0x02/pages/home.tsx
@@ -1,9 +1,23 @@
-const home: React.FC = () => {
-    return(
-        <div>
-            <h1></h1>
-        </div>
-    )
-}
+ import React from "react";
+ import Card from "@/components/common/Card";
 
-export default home;
+
+ export default function HomePage() {
+    return(
+        <div className="min-h-screen bg-gray-50 p-10">
+       <h1 className="font-bold text-white text-left">
+        Welcome to my homepage 
+        </h1>
+        <div>
+            <Card 
+            title="About us"
+            content="This is what we do."
+            />
+            <Card
+            title="Our Purpose" 
+            content="To make people come together."
+            />
+        </div>
+        </div>
+    );
+ }

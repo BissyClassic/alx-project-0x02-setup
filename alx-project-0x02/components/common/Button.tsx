5edb44f887bf708c87cf068831651f1b039733d9
BissyClassic <rachaelafolabi2021@gmail.com>
2025-09-24 14:43:33 +0100

    Add button file and updated files and folders

---
+++ alx-project-0x02/components/common/Button.tsx
@@ -0,0 +1,21 @@
+ import React from "react";
+ import { ButtonProps} from "@/interfaces";
+
+ 
+ 
+ const Button: React.FC<ButtonProps> = ({label="string", size= "medium", shape= "rounded-md" }) => {
+    const sizeClasses: Record<string, string> = {
+         small: "px-3 py-1 text-sm",
+    medium: "px-4 py-2 text-base",
+    large: "px-6 py-3 text-lg",
+    };
+
+  return (
+    <main className="flex flex-col h-screen justify-center items-center text-4xl font-semibold">
+      <h2>Click me</h2>
+      <button className=" border px-3 py-1 text-lg mt-3  bg-blue-500 text-white rounded-full">Get Started</button>
+    </main>
+  )
+}
+
+export default Button;
--- alx-project-0x02/pages/about.tsx
+++ alx-project-0x02/pages/about.tsx
@@ -1,8 +1,16 @@
-const about: React.FC = () => {
-    return(
-        <div>
-            <h1></h1>
-        </div>
-    )
-}
-export default about;
+import React from "react";
+import Button from "@/components/common/Button";
+
+    export default function AboutPage() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-10">
+      <h1 className="text-3xl font-bold mb-6">About Page</h1>
+
+      <div className="flex gap-4">
+        <Button label="Small Button" size="small" shape="rounded-sm"/>
+        <Button label="Medium Button" size="medium" shape="rounded-md" />
+        <Button label="Large Button" size="large" shape="rounded-full" />
+      </div>
+    </div>
+  );
+};
